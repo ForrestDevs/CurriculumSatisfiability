6be04319882be49811e9f95b7acef0f8b1060a9c
ForrestDevs <74693045+ForrestDevs@users.noreply.github.com>
2023-10-29 16:55:27 -0400

    Started adding some basic constraints to build theory

--- data.py
+++ data.py
@@ -1,23 +1,31 @@
 CLASSROOMS = [
     "A101", "A102", "A103", "A104", "A105", "A106", "A107", "A108", "A109", "A110",
     "B101", "B102", "B103", "B104", "B105", "B106", "B107", "B108", "B109", "B110",
-    "C101", "C102", "C103", "C104", "C105", "C106", "C107", "C108", "C109", "C110",
-    "D101", "D102", "D103", "D104", "D105", "D106", "D107", "D108", "D109", "D110",
-    "E101", "E102", "E103", "E104", "E105", "E106", "E107", "E108", "E109", "E110",
+]
+
+DAYS = [
+    "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
+]
+
+TIMESLOTS = [
+    "8AM-930AM", "930AM-11AM", "11AM-1230PM", "1230PM-2PM", ""
 ]
 
 TERMS = [
-    'Fall 2019', 'Winter 2020', 'Fall 2020', 'Winter 2021', 'Fall 2021', 'Winter 2022', 'Fall 2022', 'Winter 2023',
+    'Fall 1', 'Winter 1', 'Fall 2', 'Winter 2', 'Fall 3', 'Winter 3', 'Fall 4', 'Winter 4',
 ]
 
 PROFS = [
     "Dr. Smith", "Dr. Johnson", "Dr. Williams", "Dr. Jones", "Dr. Brown", "Dr. Davis", "Dr. Miller", "Dr. Wilson",
 ]
 
+PROGRAMS = [
+    "AI", "Bio Med", "Data Analytics", "Game Dev", "Security", "Fundamental", "Cog Sci", "Comp Sci", "Soft Design", "BioMed", "COMA", "Creative Arts"
+]
+
 COURSES = [
     "Math121", "Math122", "Math123", "Math124", "Math125", "Math126", "Math127", "Math128", "Math129", "Math130",
 ]
 
-PROGRAMS = [
-    "AI", "Bio Med", "Data Analytics", "Game Dev", "Security", "Fundamental", "Cog Sci", "Comp Sci", "Soft Design", "BioMed", "COMA", "Creative Arts"
-]
+
+
--- run.py
+++ run.py
@@ -1,15 +1,14 @@
-from data import courses, semesters, students, professors
-
+# region SETUP
+# Import necessary modules
+from data import *
 from bauhaus import Encoding, proposition, constraint
 from bauhaus.utils import count_solutions, likelihood
-
 # These two lines make sure a faster SAT solver is used.
 from nnf import config
 config.sat_backend = "kissat"
-
 # Encoding that will store all of your constraints
 E = Encoding()
-
+# A base class to implement hashable
 class Hashable:
     def __hash__(self):
         return hash(str(self))
@@ -19,10 +18,10 @@ class Hashable:
 
     def __repr__(self):
         return str(self)
+# endregion SETUP
 
-# region Propositions
-#############################################################################################################
-# EXAMPLES:
+
+# region EXAMPLES
 #############################################################################################################
 # To create propositions, create classes for them first, annotated with "@proposition" and the Encoding
 @proposition(E)
@@ -49,22 +48,27 @@ class FancyPropositions:
     def __repr__(self):
         return f"A.{self.data}"
     
-#############################################################################################################
-# Define your propositions here:
-#############################################################################################################
+#############################################################################################################  
+# endregion EXAMPLES
+  
 
+# region Propositions
+#############################################################################################################
+# Professor Props:
+#############################################################################################################
 @proposition(E)
 class Professor_Available(Hashable):
-    def __init__(self, professor, term):
+    def __init__(self, professor, term, day, time):
         self.professor = professor
         self.term = term
+        self.day = day
+        self.time = time
 
     def __repr__(self):
-        return f"Professor_Available(professor={self.professor}, term={self.term})"
-
+        return f"Professor_Available(professor={self.professor}, term={self.term}, day={self.day}, time={self.time})"
 
 @proposition(E)
-class Professor_Assigned:
+class Professor_Assigned(Hashable):
     def __init__(self, professor, course, term):
         self.professor = professor
         self.course = course
@@ -72,7 +76,40 @@ class Professor_Assigned:
 
     def __repr__(self):
         return f"Professor_Assigned(professor={self.professor}, course={self.course}, term={self.term})"
+#############################################################################################################
+# Course Props:
+#############################################################################################################
+@proposition(E)
+class Course_Assigned_DT(Hashable):
+    def __init__(self, course, term, day, time):
+        self.course = course
+        self.term = term
+        self.day = day
+        self.time = time
 
+    def __repr__(self):
+        return f"Course_Assigned_DT(course={self.course}, term={self.term}, day={self.day}, time={self.time})"
+    
+@proposition(E)
+class CourseAssigned:
+    def __init__(self, course, room, term, day, time) -> None:
+        self.course = course
+        self.room = room
+        self.term = term
+        self.day = day
+        self.time = time
+
+    def __repr__(self) -> str:
+        return f"CourseAssigned(course={self.course}, room={self.room}, term={self.term}, day={self.day}, time={self.time})"
+    
+@proposition(E)
+class Course_Prerequisite:
+    def __init__(self, course, prerequisite):
+        self.course = course
+        self.prerequisite = prerequisite
+
+    def __repr__(self):
+        return f"Course_Prerequisite(course={self.course}, prerequisite={self.prerequisite})"
 
 @proposition(E)
 class Course_Offered:
@@ -92,18 +129,9 @@ class Course_Can_Be_Scheduled:
 
     def __repr__(self):
         return f"Course_Can_Be_Scheduled(course={self.course}, term={self.term})"
-
-
-@proposition(E)
-class Course_Prerequisite:
-    def __init__(self, course, prerequisite):
-        self.course = course
-        self.prerequisite = prerequisite
-
-    def __repr__(self):
-        return f"Course_Prerequisite(course={self.course}, prerequisite={self.prerequisite})"
-
-
+#############################################################################################################
+# Program Props:
+#############################################################################################################
 @proposition(E)
 class Program_Req_Course:
     def __init__(self, course, program):
@@ -133,80 +161,133 @@ class Program_Can_Complete:
         
     def __repr__(self):
         return f"Program_Can_Complete(program={self.program}, term={self.term})"
-
-
+#############################################################################################################
+# Classroom Props:
+#############################################################################################################
 @proposition(E)
-class Classroom_Free:
-    def __init__(self, room, term):
+class Classroom_Available(Hashable):
+    def __init__(self, room, term, day, time) -> None:
         self.room = room
         self.term = term
+        self.day = day
+        self.time = time
 
-    def __repr__(self):
-        return f"Classroom_Free(room={self.room}, term={self.term})"
-
-
-@proposition(E)
-class Course_Assigned_Room:
-    def __init__(self, course, room, term):
-        self.course = course
-        self.room = room
-        self.term = term
-
-    def __repr__(self):
-        return f"Course_Assigned_Room(course={self.course}, room={self.room}, term={self.term})"
+    def __repr__(self) -> str:
+        return f"Classroom_Free(room={self.room}, term={self.term}, day={self.day}, time={self.time})"
 # endregion
 
 
-# Call your variables whatever you want
-a = BasicPropositions("a")
-b = BasicPropositions("b")
-c = BasicPropositions("c")
-d = BasicPropositions("d")
-e = BasicPropositions("e")
-# At least one of these will be true
-x = FancyPropositions("x")
-y = FancyPropositions("y")
-z = FancyPropositions("z")
+# region INIT Props
+# Define program requirements using Program_Req_Course propositions
+# prog_req_props = []
+# for program in PROGRAMS:
+#     for course in program.requirements: 
+#         prog_req_props.append(Program_Req_Course(course, program))
 
 
+# Define professor and classroom availability using Professor_Available and Classroom_Available propositions
+prof_avail_props = []
+for professor in PROFS:
+    for term in TERMS:
+        for day in DAYS:
+            for time in TIMESLOTS:
+                prof_avail_props.append(Professor_Available(professor, term, day, time))
 
-# E.add_constraint(Course_Assigned_Room(course, room, term) >> Course_Offered(course, term))
-# E.add_constraint(Course_Assigned_Room(course, room, term) >> Classroom_Free(professor, course, term))
+room_avail_props = []                
+for room in CLASSROOMS:
+    for term in TERMS:
+        for day in DAYS:
+            for time in TIMESLOTS:
+                room_avail_props.append(Classroom_Available(room, term, day, time))
+                
+# Define course prerequisites using Course_Prerequisite propositions
 
 
-# Build an example full theory for your setting and return it.
-#
+# Define course scheduling using Course_Assigned_DT and Course_Assigned_Room propositions
+course_assigned_props = []
+for course in COURSES:
+    for term in TERMS:
+        for room in CLASSROOMS:
+            for day in DAYS:
+                for time in TIMESLOTS:
+                    course_assigned_props.append(CourseAssigned(course, room, term, day, time))
+# endregion INIT Props
+
+
+# region Add Constraints + Build Theory
+#  Build the theory
 #  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
 #  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
 #  what the expectations are.
-def example_theory():
-    # Add custom constraints by creating formulas with the variables you created.
-    E.add_constraint((a | b) & ~x)
-    # Implication
-    E.add_constraint(y >> z)
-    # Negate a formula
-    E.add_constraint(~(x & y))
-    # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
-    # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
-    constraint.add_exactly_one(E, a, b, c)
+def schedule_programs():
+
+    # Let's ensure that a course can't be assigned to a classroom that already has a course scheduled at a specific day and time.
+    classroom_assignments = {}
+    for assignment in course_assigned_props:
+        classroom = assignment.room
+        day = assignment.day
+        time = assignment.time
+
+        if classroom not in classroom_assignments:
+            classroom_assignments[classroom] = {}
+
+        if day not in classroom_assignments[classroom]:
+            classroom_assignments[classroom][day] = {}
+
+        if time not in classroom_assignments[classroom][day]:
+            classroom_assignments[classroom][day][time] = []
+        
+        classroom_assignments[classroom][day][time].append(assignment.course)
+
+    for classroom, days in classroom_assignments.items():
+        for day, times in days.items():
+            for time, courses in times.items():
+                if len(courses) > 1:
+                    for i in range(len(courses)):
+                        for j in range(i + 1, len(courses)):
+                            # Ensure that no two courses are assigned to the same classroom at the same day and time
+                            constraint.add_exactly_one(E, courses[i], courses[j])
+
+
+    # Create a dictionary to keep track of professor assignments for each day and time
+    professor_assignments = {}
+    # Assume you have propositions for Professor_Assigned and Course_Assigned_DT
+    # Replace these with your actual propositions
+    # For each course assigned to a professor, add it to the professor_assignments list
+    for assignment in Professor_Assigned.instances:
+        professor = assignment.professor
+        day = assignment.day
+        time = assignment.time
+
+        if professor not in professor_assignments:
+            professor_assignments[professor] = {}
+
+        if day not in professor_assignments[professor]:
+            professor_assignments[professor][day] = {}
+
+        if time not in professor_assignments[professor][day]:
+            professor_assignments[professor][day][time] = []
+
+        professor_assignments[professor][day][time].append(assignment.course)
+    # Now, create a constraint to ensure a professor is not assigned to more than one place at the same time
+    for professor, days in professor_assignments.items():
+        for day, times in days.items():
+            for time, courses in times.items():
+                if len(courses) > 1:
+                    for i in range(len(courses)):
+                        for j in range(i + 1, len(courses)):
+                            # Ensure that no two courses are assigned to the same professor at the same day and time
+                            constraint.add_exactly_one(E, courses[i], courses[j])
 
     return E
+# endregion Add Constraints + Build Theory
 
 
+# region Compile + Run
 if __name__ == "__main__":
-
-    T = example_theory()
-    # Don't compile until you're finished adding all your constraints!
+    T = schedule_programs()
     T = T.compile()
-    # After compilation (and only after), you can check some of the properties
-    # of your model:
     print("\nSatisfiable: %s" % T.satisfiable())
     print("# Solutions: %d" % count_solutions(T))
     print("   Solution: %s" % T.solve())
-
-    print("\nVariable likelihoods:")
-    for v, vn in zip([a, b, c, x, y, z], 'abcxyz'):
-        # Ensure that you only send these functions NNF formulas
-        # Literals are compiled to NNF here
-        print(" %s: %.2f" % (vn, likelihood(T, v)))
-    print()
+# endregion Compile + Run
