0dca4745625954493a5cffda79f85fc3b2a08c1d
ForrestDevs <74693045+ForrestDevs@users.noreply.github.com>
2023-10-29 18:53:17 -0400

    Added some more constraints

--- data.py
+++ data.py
@@ -1,42 +1,120 @@
+# List of classrooms available to schedule lectures in 
 CLASSROOMS = [
     "A101", "A102", "A103", "A104", "A105", "A106", "A107", "A108", "A109", "A110",
     "B101", "B102", "B103", "B104", "B105", "B106", "B107", "B108", "B109", "B110",
 ]
-
+# List of days that lectures can be scheduled
 DAYS = [
     "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
 ]
-
+# List of times that a lecture can be scheduled on
 TIMESLOTS = [
     "8AM-930AM", "930AM-11AM", "11AM-1230PM", "1230PM-2PM", ""
 ]
-
+# List of the 8 different terms
 TERMS = [
     'Fall 1', 'Winter 1', 'Fall 2', 'Winter 2', 'Fall 3', 'Winter 3', 'Fall 4', 'Winter 4',
 ]
-
+# List of all the Professors
 PROFS = [
     "Dr. Smith", "Dr. Johnson", "Dr. Williams", "Dr. Jones", "Dr. Brown", "Dr. Davis", "Dr. Miller", "Dr. Wilson",
 ]
-
-PROGRAMS = {
-    "AI": [], 
-    "Bio Med": [], 
-    "Data Analytics": [], 
-    "Game Dev": [], 
-    "Security": [], 
-    "Fundamental": [], 
-    "Cog Sci": [], 
-    "Comp Sci": [], 
-    "Soft Design": [], 
-    "BioMed": [], 
-    "COMA": [], 
-    "Creative Arts": []
+# Dict of all the Professors with the courses they're qualified to teach
+PROFS_QUAL = {
+    "Dr. Smith": [], 
+    "Dr. Johnson": [], 
+    "Dr. Williams": [], 
+    "Dr. Jones": [],
+    "Dr. Brown": [], 
+    "Dr. Davis": [], 
+    "Dr. Miller": [], 
+    "Dr. Wilson": [],
 }
-
+# List of all the Courses
 COURSES = [
     "Math121", "Math122", "Math123", "Math124", "Math125", "Math126", "Math127", "Math128", "Math129", "Math130",
 ]
+# Dict of all the Courses with the prerequisites required
+COURSE_REQS = {
+    "CISC121": [],
+    "CISC124": ["CISC121"]
+}
+# Nested Dict of All the Programs, with the requirements broken down by Year 
+PROGRAMS = {
+    "AI": {
+        "Year1": ["AI101", "AI102", "AI103"],
+        "Year2": ["AI201", "AI202", "AI203"],
+        "Year3": ["AI301", "AI302", "AI303"],
+        "Year4": ["AI401", "AI402", "AI403"],
+    },
+    "Bio Med": {
+        "Year1": [],
+        "Year2": [],
+        "Year3": [],
+        "Year4": [],
+    },
+    "Data Analytics": {
+        "Year1": [],
+        "Year2": [],
+        "Year3": [],
+        "Year4": [],
+    }, 
+    "Game Dev": {
+        "Year1": [],
+        "Year2": [],
+        "Year3": [],
+        "Year4": [],
+    }, 
+    "Security": {
+        "Year1": [],
+        "Year2": [],
+        "Year3": [],
+        "Year4": [],
+    }, 
+    "Fundamental": {
+        "Year1": [],
+        "Year2": [],
+        "Year3": [],
+        "Year4": [],
+    }, 
+    "Cog Sci": {
+        "Year1": [],
+        "Year2": [],
+        "Year3": [],
+        "Year4": [],
+    }, 
+    "Comp Sci": {
+        "Year1": [],
+        "Year2": [],
+        "Year3": [],
+        "Year4": [],
+    }, 
+    "Soft Design": {
+        "Year1": [],
+        "Year2": [],
+        "Year3": [],
+        "Year4": [],
+    }, 
+    "BioMed": {
+        "Year1": [],
+        "Year2": [],
+        "Year3": [],
+        "Year4": [],
+    }, 
+    "COMA": {
+        "Year1": [],
+        "Year2": [],
+        "Year3": [],
+        "Year4": [],
+    }, 
+    "Creative Arts": {
+        "Year1": [],
+        "Year2": [],
+        "Year3": [],
+        "Year4": [],
+    }
+}
+
 
 
 
--- run.py
+++ run.py
@@ -57,41 +57,21 @@ class FancyPropositions:
 # Professor Props:
 #############################################################################################################
 @proposition(E)
-class Professor_Available(Hashable):
-    def __init__(self, professor, term, day, time):
+class ProfessorAssigned(Hashable):
+    def __init__(self, professor, course, term, day, time) -> None:
         self.professor = professor
+        self.course = course
         self.term = term
         self.day = day
         self.time = time
 
-    def __repr__(self):
-        return f"Professor_Available(professor={self.professor}, term={self.term}, day={self.day}, time={self.time})"
-
-@proposition(E)
-class Professor_Assigned(Hashable):
-    def __init__(self, professor, course, term):
-        self.professor = professor
-        self.course = course
-        self.term = term
-
-    def __repr__(self):
-        return f"Professor_Assigned(professor={self.professor}, course={self.course}, term={self.term})"
+    def __repr__(self) -> str:
+        return f"Professor_Assigned(professor={self.professor}, course={self.course}, term={self.term}, day={self.day}, time={self.time})"
 #############################################################################################################
 # Course Props:
 #############################################################################################################
 @proposition(E)
-class Course_Assigned_DT(Hashable):
-    def __init__(self, course, term, day, time):
-        self.course = course
-        self.term = term
-        self.day = day
-        self.time = time
-
-    def __repr__(self):
-        return f"Course_Assigned_DT(course={self.course}, term={self.term}, day={self.day}, time={self.time})"
-    
-@proposition(E)
-class CourseAssigned:
+class CourseAssigned(Hashable):
     def __init__(self, course, room, term, day, time) -> None:
         self.course = course
         self.room = room
@@ -101,34 +81,45 @@ class CourseAssigned:
 
     def __repr__(self) -> str:
         return f"CourseAssigned(course={self.course}, room={self.room}, term={self.term}, day={self.day}, time={self.time})"
+# @proposition(E)
+# class Course_Assigned_DT(Hashable):
+#     def __init__(self, course, term, day, time):
+#         self.course = course
+#         self.term = term
+#         self.day = day
+#         self.time = time
+
+#     def __repr__(self):
+#         return f"Course_Assigned_DT(course={self.course}, term={self.term}, day={self.day}, time={self.time})"
     
-@proposition(E)
-class Course_Prerequisite:
-    def __init__(self, course, prerequisite):
-        self.course = course
-        self.prerequisite = prerequisite
 
-    def __repr__(self):
-        return f"Course_Prerequisite(course={self.course}, prerequisite={self.prerequisite})"
+# @proposition(E)
+# class Course_Prerequisite:
+#     def __init__(self, course, prerequisite):
+#         self.course = course
+#         self.prerequisite = prerequisite
 
-@proposition(E)
-class Course_Offered:
-    def __init__(self, course, term):
-        self.course = course
-        self.term = term
+#     def __repr__(self):
+#         return f"Course_Prerequisite(course={self.course}, prerequisite={self.prerequisite})"
 
-    def __repr__(self):
-        return f"Course_Offered(course={self.course}, term={self.term})"
+# @proposition(E)
+# class Course_Offered:
+#     def __init__(self, course, term):
+#         self.course = course
+#         self.term = term
+
+#     def __repr__(self):
+#         return f"Course_Offered(course={self.course}, term={self.term})"
 
 
-@proposition(E)
-class Course_Can_Be_Scheduled:
-    def __init__(self, course, term):
-        self.course = course
-        self.term = term
+# @proposition(E)
+# class Course_Can_Be_Scheduled:
+#     def __init__(self, course, term):
+#         self.course = course
+#         self.term = term
 
-    def __repr__(self):
-        return f"Course_Can_Be_Scheduled(course={self.course}, term={self.term})"
+#     def __repr__(self):
+#         return f"Course_Can_Be_Scheduled(course={self.course}, term={self.term})"
 #############################################################################################################
 # Program Props:
 #############################################################################################################
@@ -165,7 +156,7 @@ class Program_Can_Complete:
 # Classroom Props:
 #############################################################################################################
 @proposition(E)
-class Classroom_Available(Hashable):
+class ClassroomAvailable(Hashable):
     def __init__(self, room, term, day, time) -> None:
         self.room = room
         self.term = term
@@ -173,7 +164,7 @@ class Classroom_Available(Hashable):
         self.time = time
 
     def __repr__(self) -> str:
-        return f"Classroom_Free(room={self.room}, term={self.term}, day={self.day}, time={self.time})"
+        return f"ClassroomAvailable(room={self.room}, term={self.term}, day={self.day}, time={self.time})"
 # endregion
 
 
@@ -191,19 +182,19 @@ for professor in PROFS:
     for term in TERMS:
         for day in DAYS:
             for time in TIMESLOTS:
-                prof_avail_props.append(Professor_Available(professor, term, day, time))
+                prof_avail_props.append(ProfessorAssigned(professor, term, day, time))
 
 room_avail_props = []                
 for room in CLASSROOMS:
     for term in TERMS:
         for day in DAYS:
             for time in TIMESLOTS:
-                room_avail_props.append(Classroom_Available(room, term, day, time))
+                room_avail_props.append(ClassroomAvailable(room, term, day, time))
                 
 # Define course prerequisites using Course_Prerequisite propositions
 
 
-# Define course scheduling using Course_Assigned_DT and Course_Assigned_Room propositions
+# Define course scheduling using CourseAssigned propositions
 course_assigned_props = []
 for course in COURSES:
     for term in TERMS:
@@ -220,64 +211,93 @@ for course in COURSES:
 #  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
 #  what the expectations are.
 def schedule_programs():
+    # TODO: Make sure that prerequisite courses are scheduled in a term before the course that requires them
+    for course, prerequisites in COURSE_REQS.items():
+        for term in TERMS:
+            for prerequisite in prerequisites:
+                for room in CLASSROOMS:
+                    for day in DAYS:
+                        for time in TIMESLOTS:
+                             # Ensure that the prerequisite course is offered in the term before the required course
+                            E.add_constraint(~CourseAssigned(prerequisite, room, term, day, time) | 
+                                             CourseAssigned(course, room, term, day, time))
+                            
 
-    # Let's ensure that a course can't be assigned to a classroom that already has a course scheduled at a specific day and time.
+    # TODO: Make sure that a course can't be assigned to a classroom that already has a course scheduled at a specific day and time.
+    # Create a dictionary to keep track of classroom assignments
     classroom_assignments = {}
     for assignment in course_assigned_props:
-        classroom = assignment.room
+        course = assignment.course
+        room = assignment.room
         day = assignment.day
         time = assignment.time
 
-        if classroom not in classroom_assignments:
-            classroom_assignments[classroom] = {}
+        if room not in classroom_assignments:
+            classroom_assignments[room] = {}
 
-        if day not in classroom_assignments[classroom]:
-            classroom_assignments[classroom][day] = {}
+        if day not in classroom_assignments[room]:
+            classroom_assignments[room][day] = {}
 
-        if time not in classroom_assignments[classroom][day]:
-            classroom_assignments[classroom][day][time] = []
-        
-        classroom_assignments[classroom][day][time].append(assignment.course)
+        if time not in classroom_assignments[room][day]:
+            classroom_assignments[room][day][time] = []
 
-    for classroom, days in classroom_assignments.items():
+        classroom_assignments[room][day][time].append(course)
+    # Add constraints to ensure that no two courses are assigned to the same classroom at the same day and time
+    for room, days in classroom_assignments.items():
         for day, times in days.items():
             for time, courses in times.items():
                 if len(courses) > 1:
                     for i in range(len(courses)):
                         for j in range(i + 1, len(courses)):
-                            # Ensure that no two courses are assigned to the same classroom at the same day and time
-                            constraint.add_exactly_one(E, courses[i], courses[j])
+                            E.add_constraint(~courses[i] | ~courses[j])
 
 
-    # Create a dictionary to keep track of professor assignments for each day and time
+    # TODO: Make sure that there are at least 2 lectures per course
+    for course in COURSES:
+        for term in TERMS: 
+            lectures = [CourseAssigned(course, _, term, _, _).var for _ in DAYS for _ in TIMESLOTS]
+            E.add_constraint(sum(lectures) >= 2)
+
+
+    # TODO: Make sure that a professor is not assigned to more than once place at the same time
     professor_assignments = {}
-    # Assume you have propositions for Professor_Assigned and Course_Assigned_DT
-    # Replace these with your actual propositions
-    # For each course assigned to a professor, add it to the professor_assignments list
-    for assignment in Professor_Assigned.instances:
+    for professor in PROFS:
+        professor_assignments[professor] = []
+    for assignment in prof_avail_props:
         professor = assignment.professor
         day = assignment.day
         time = assignment.time
 
-        if professor not in professor_assignments:
-            professor_assignments[professor] = {}
+        if (day, time) in professor_assignments[professor]:
+            # If a professor is already assigned at the same day and time, add a constraint
+            E.add_constraint(~assignment)
+        
+        professor_assignments[professor].append((day, time))
 
-        if day not in professor_assignments[professor]:
-            professor_assignments[professor][day] = {}
 
-        if time not in professor_assignments[professor][day]:
-            professor_assignments[professor][day][time] = []
+    # TODO: Make sure that Profs can only teach courses for which they're qualified
+    for professor in PROFS: 
+        qualified_courses = PROFS_QUAL[professor]
+        for course in COURSES:
+            for term in TERMS:
+                for day in DAYS:
+                    for time in TIMESLOTS:
+                        if course not in qualified_courses:
+                            # If the course is not in the list of qualified courses for the professor,
+                            # then ensure that the professor is not assigned to teach that course.
+                            E.add_constraint(~(ProfessorAssigned(professor, course, term, day, time)))
 
-        professor_assignments[professor][day][time].append(assignment.course)
-    # Now, create a constraint to ensure a professor is not assigned to more than one place at the same time
-    for professor, days in professor_assignments.items():
-        for day, times in days.items():
-            for time, courses in times.items():
-                if len(courses) > 1:
-                    for i in range(len(courses)):
-                        for j in range(i + 1, len(courses)):
-                            # Ensure that no two courses are assigned to the same professor at the same day and time
-                            constraint.add_exactly_one(E, courses[i], courses[j])
+
+    # TODO: Make sure that all required Courses for the Year Are Scheduled 
+    for program, year_reqs in PROGRAMS.items():
+        for year, req_course in year_reqs.items():
+            for course in req_course:
+                for term in TERMS:
+                    if course not in COURSES:
+                        # If the required course is not in the list of all courses,
+                        # then ensure that it is not scheduled in that term.
+                         E.add_constraint(~(CourseAssigned(course, _, term, _, _).var for _ in DAYS for _ in TIMESLOTS))
+
 
     return E
 # endregion Add Constraints + Build Theory
