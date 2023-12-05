8cb8b07c4d054ab60bbfc7e5a650c56165720c62
kareem717 <kareem.717@icloud.com>
2023-12-05 17:42:44 -0500

    Seperate propositions to different module

--- run.py
+++ run.py
@@ -5,128 +5,16 @@ from itertools import combinations, product
 from collections import defaultdict
 from bauhaus import Encoding, proposition, constraint, Or, And
 from bauhaus.utils import count_solutions, likelihood
-
+from propositions import *
 from nnf import config
+
+# Configure SAT backend
 config.sat_backend = "kissat"
-
-E = Encoding()
-
-class Hashable:
-    def __hash__(self):
-        return hash(str(self))
-
-    def __eq__(self, __value: object) -> bool:
-        return hash(self) == hash(__value)
-
-    def __repr__(self):
-        return str(self)
-
-# Professor Props:
-@proposition(E)
-class ProfessorAssigned(Hashable):
-    def __init__(self, professor, course, term, day, time) -> None:
-        self.professor = professor
-        self.course = course
-        self.term = term
-        self.day = day
-        self.time = time
-
-    def __repr__(self) -> str:
-        return f"Professor_Assigned(professor={self.professor}, course={self.course}, term={self.term}, day={self.day}, time={self.time})"
-
-@proposition(E)
-class ProfessorQualified(Hashable): 
-    def __init__(self, professor, course)-> None:
-        self.professor = professor
-        self.course = course
-
-    def __repr__(self) -> str:
-        return f"ProfessorQualifiedForCourse(professor={self.professor}, course={self.course})"
-
-# Course Props:
-@proposition(E)
-class CourseAssigned(Hashable):
-    def __init__(self, course, room, term, day, time) -> None:
-        self.course = course
-        self.room = room
-        self.term = term
-        self.day = day
-        self.time = time
-
-    def __repr__(self) -> str:
-        return f"CourseAssigned(course={self.course}, room={self.room}, term={self.term}, day={self.day}, time={self.time})"
-
-@proposition(E)
-class CoursePrerequisite(Hashable):
-    def __init__(self, course, prerequisite) -> None:
-        self.course = course
-        self.prerequisite = prerequisite
-
-    def __repr__(self) -> str:
-        return f"Course_Prerequisite(course={self.course}, prerequisite={self.prerequisite})"
-
-# Program Props:
-@proposition(E)
-class ProgramReqCourse:
-    def __init__(self, course, program, year):
-        self.course = course
-        self.program = program
-        self.year = year
-
-    def __repr__(self):
-        return f"Program_Req_Course(course={self.course}, program={self.program}, year={self.year})"
-
-@proposition(E)
-class ProgramSharesPreReq:
-    def __init__(self, program1, program2, course):
-        self.program1 = program1
-        self.program2 = program2
-        self.course = course
-
-    def __repr__(self):
-        return f"Program_Shares_PreReq(program1={self.program1}, program2={self.program2}, course={self.course})"
-
-@proposition(E)
-class ProgramCanComplete:
-    def __init__(self, program, term):
-        self.program = program
-        self.term = term
-        
-    def __repr__(self):
-        return f"Program_Can_Complete(program={self.program}, term={self.term})"
-
-
-# Classroom Props:
-@proposition(E)
-class ClassroomAssigned(Hashable):
-    def __init__(self, room, course, term, day, time) -> None:
-        self.room = room
-        self.course = course
-        self.term = term
-        self.day = day
-        self.time = time
-
-    def __repr__(self) -> str:
-        return f"ClassroomAssigned(room={self.room}, course={self.course}, term={self.term}, day={self.day}, time={self.time})"
 
 # Define course scheduling using CourseAssigned propositions
 course_assigned_props = list()
 for course, term, room, day, time in tqdm(product(COURSES.keys(), TERMS, CLASSROOMS, DAYS, TIMESLOTS), desc="Adding course assigned propositions"):
     course_assigned_props.append(CourseAssigned(course, room, term, day, time))
-
-# Define course prerequisites using CoursePrerequisite propositions
-course_prerequisite_props = list()
-for course, value in tqdm(COURSES.items(), desc="Adding course prerequisite propositions"):
-    print(course, value)
-    if value['reqs']:
-        print("REQS: ", value['reqs'])
-        for prerequisite in value['reqs']:
-            if prerequisite:
-                course_prerequisite_props.append(CoursePrerequisite(course, prerequisite))
-
-
-# Create the CourseAssigned propositions with the corresponding variables
-course_assigned_props = [CourseAssigned(course, room, term, day, time) for course, term, room, day, time in product(COURSES.keys(), TERMS, CLASSROOMS, DAYS, TIMESLOTS)]
 
 # Define course prerequisites using CoursePrerequisite propositions
 course_prerequisite_props = list()
@@ -176,6 +64,8 @@ def schedule_programs():
         # Add a constraint that exactly one of these pair constraints is true
         constraint.add_exactly_one(E, pair_constraints)
 
+    # Ensure there are no classroom conflicts
+    
     return E
 
 
